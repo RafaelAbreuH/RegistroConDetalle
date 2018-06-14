6dfe9707d2a2dc1b621ae89d410e7ae6babb58e7
Rafael Abreu Hernandez <Rafael_abreu5@ucne.edu.do>
2018-06-13 21:28:15 -0400

    BLL, DAL, Entidades Agregado.

---
+++ RegistroConDetalle/DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegistroConDetalle.Entidades;
+using System.Data.Entity;
+
+namespace RegistroConDetalle.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Grupos> Grupos { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ RegistroConDetalle/Entidades/Grupos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using RegistroConDetalle.Entidades;
+
+namespace RegistroConDetalle.Entidades
+{
+    public class Grupos
+    {
+        [Key]
+        public int GrupoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public String Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public int Grupo { get; set; }
+        public int Integrantes { get; set; }
+
+        [StringLength(100)]
+        public string Comments { get; set; }
+
+        public virtual ICollection<GruposDetalle> Detalle { get; set; }
+
+        public Grupos()
+        {
+            //Inicializar la Lista
+            this.Detalle = new List<GruposDetalle>();
+        }
+
+        public void AgregarDetalle(int id, int gruposId, int personasId, string cargo)
+        {
+            this.Detalle.Add(new GruposDetalle(id, gruposId, personasId, cargo));
+        }
+    }
+}
